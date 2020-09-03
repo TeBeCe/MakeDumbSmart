1a380d3964344417a727f2da00a1510e27ba116d
Martin Bachraty <matbachraty@gmail.com>
2020-09-03 17:23:08 +0200

    Initial Commit

---
+++ MakeDumbSmart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeDumbSmart
+//
+//  Created by Martin Bachratý on 03/09/2020.
+//  Copyright © 2020 Martin Bachratý. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
