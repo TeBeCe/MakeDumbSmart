f49d7faa62bdabd17de1c0c62502f3f200f03762
Martin Bachraty <matbachraty@gmail.com>
2020-09-03 17:30:42 +0200

    Iniital commit

---
+++ Model/DeviceModel.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceModel.swift
+//  MakeDumbSmart
+//
+//  Created by Martin Bachratý on 03/09/2020.
+//  Copyright © 2020 Martin Bachratý. All rights reserved.
+//
+
+import Foundation
---
+++ ViewModel/DeviceViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceViewModel.swift
+//  MakeDumbSmart
+//
+//  Created by Martin Bachratý on 03/09/2020.
+//  Copyright © 2020 Martin Bachratý. All rights reserved.
+//
+
+import Foundation
